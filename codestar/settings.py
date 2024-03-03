a9c7562f893c90492bea04082a8350655e4ccd34
Liam G <liamg10@yahoo.co.uk>
2024-03-03 21:49:07 +0000

    add pagenation

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-caleom-djangoblog-pmxs73yanjp.ws-eu108.gitpod.io',
+ALLOWED_HOSTS = ['.gitpod.io',
                 '.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = ['https://8000-caleom-djangoblog-pmxs73yanjp.ws-eu108.gitpod.io',
+CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io',
                     "https://*.herokuapp.com"]
 
 
